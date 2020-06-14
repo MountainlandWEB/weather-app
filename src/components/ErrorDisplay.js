ac6868cdd283978e9fa960ded4bf2c4d12dfec33
Trevor Michaelis <trevor.michaelis@vivint.com>
2020-06-13 18:13:00 -0600

    tm: add error display styling

--- src/components/ErrorDisplay.js
+++ src/components/ErrorDisplay.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 class ErrorDisplay extends React.Component {
   render() {
-    console.log(this.props)
     return (
-      <div>{ this.props.message }</div>
+      <div className="error-display">
+        <div className="flex-row">
+          <div className="error-display-card">
+            { this.props.message }
+          </div>
+        </div>
+      </div>
     )
   }
 }
